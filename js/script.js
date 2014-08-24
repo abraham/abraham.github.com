6d8eb98b20c4946c1b7319a2870ef10f382aa89b
Abraham Williams <4braham@gmail.com>
2014-08-24 14:39:15 -0700

    more loading optimizations

--- js/script.js
+++ js/script.js
@@ -18,27 +18,38 @@ _gaq.push(['_trackPageLoadTime']);
 })();
 
 
+// Google Fonts
+(function() {
+  var l = document.createElement('link'); l.rel = 'stylesheet';
+  l.href = 'https://fonts.googleapis.com/css?family=Roboto';
+  var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+})();
+
+
 // Trigger install of Chrome extension
+function cwsClickHandler(event) {
+    chrome.webstore.install(event.currentTarget.getAttribute('href'));
+    event.preventDefault();
+}
 if (typeof chrome === 'object' && typeof chrome.webstore === 'object' && typeof chrome.webstore.install === 'function') {
   var cwsItems = document.getElementsByClassName("chrome-web-store");
   for (var i = 0; i < cwsItems.length; i++) {
-    cwsItems[i].addEventListener('click', function(event){
-      chrome.webstore.install(event.currentTarget.getAttribute('href'));
-      event.preventDefault();
-    });
+    cwsItems[i].addEventListener('click', cwsClickHandler);
   }
 }
 
 
+// Load higher resolution background image
 setTimeout(function() {
     var imageObj = new Image();
     imageObj.src = '/img/lake.jpg';
     imageObj.onload= function() {
       document.body.style.backgroundImage = "url('/img/lake.jpg')";
-    }
+    };
 }, 0);
 
 
+// Page is ready to be displayed
 document.addEventListener("DOMContentLoaded", function(event) {
     setTimeout(function() {
         document.body.classList.remove('blur');
