2e7c7c5c2c00eb3010fd5ac85bc4729f8d664c17
Abraham Williams <4braham@gmail.com>
2016-11-15 10:29:15 -0800

    Switch to hosting css/fonts on firebase

--- sw.js
+++ sw.js
@@ -3,9 +3,10 @@ var urlsToCache = [
   '/',
 
   'https://fonts.googleapis.com/css?family=Roboto',
-  'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-  'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-  'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0',
+  '/css/bootstrap.min.css',
+  '/css/font-awesome.min.css',
+
+  '/fonts/fontawesome-webfont.woff2?v=4.7.0',
 
   '/img/moon-xsmall.jpg',
   '/img/moon.jpg',
