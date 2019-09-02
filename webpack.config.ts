bc5582d2aa2b4c1c0b760fbbcabf5d5167c25d5f
Abraham Williams <4braham@gmail.com>
2019-09-02 11:05:51 -0500

    Use sassOptions

--- webpack.config.ts
+++ webpack.config.ts
@@ -92,7 +92,9 @@ module.exports = {
           {
             loader: 'sass-loader',
             options: {
-              includePaths: [path.resolve(__dirname, 'node_modules')],
+              sassOptions: {
+                includePaths: [path.resolve(__dirname, 'node_modules')],
+              },
             },
           },
         ],
